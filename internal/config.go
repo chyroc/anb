e90e5b9e3624c2808c88efeb0916e69ac1ec6080
chyroc <chyroc@qq.com>
2021-10-05 23:57:51 +0800

    :wrench: chore: init config

---
+++ internal/config.go
@@ -0,0 +1,85 @@
+package internal
+
+import (
+	"fmt"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
+)
+
+func LoadConfig(file string) (*Config, error) {
+	bs, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	conf := new(Config)
+	err = yaml.Unmarshal(bs, conf)
+	if err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+type Config struct {
+	Server *ConfigServer `yaml:"server"`
+	Tasks  []*ConfigTask `yaml:"tasks"`
+}
+
+type ConfigServer struct {
+	User string `yaml:"user"`
+	Host string `yaml:"host"`
+}
+
+type ConfigTask struct {
+	Name string          `yaml:"name"`
+	Copy *ConfigTaskCopy `yaml:"copy"`
+	Cmd  *ConfigTaskCmd  `yaml:"cmd"`
+}
+
+type ConfigTaskCopy struct {
+	Src  string `yaml:"src"`
+	Dest string `yaml:"dest"`
+}
+
+type ConfigTaskCmd struct {
+	Commands []string
+}
+
+type TaskType string
+
+const (
+	TaskTypeCopy TaskType = "copy"
+	TaskTypeCmd  TaskType = "cmd"
+)
+
+func (r *ConfigTask) TaskType() TaskType {
+	if r.Copy != nil {
+		return TaskTypeCopy
+	}
+	if r.Cmd != nil {
+		return TaskTypeCmd
+	}
+	return ""
+}
+
+func (r *ConfigTaskCmd) UnmarshalYAML(unmarshal func(interface{}) error) error {
+	{
+		var data []string
+		if err := unmarshal(&data); err == nil {
+			r.Commands = data
+			return nil
+		}
+	}
+	{
+		var data string
+		if err := unmarshal(&data); err == nil {
+			r.Commands = append(r.Commands, data)
+			return nil
+		}
+	}
+	return fmt.Errorf("不支持的 cmd 命令")
+}
+
+func (r ConfigTaskCmd) MarshalYAML() (interface{}, error) {
+	return yaml.Marshal(r.Commands)
+}
---
+++ internal/config_test.go
@@ -0,0 +1,45 @@
+package internal
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Config(t *testing.T) {
+	as := assert.New(t)
+	chTestDir()
+
+	t.Run("", func(t *testing.T) {
+		conf, err := LoadConfig("testdata/config.1.yaml")
+		as.Nil(err)
+		as.Equal("root", conf.Server.User)
+		as.Equal("1.2.3.4", conf.Server.Host)
+
+		{
+			as.Equal("x", conf.Tasks[0].Name)
+			as.Equal("a", conf.Tasks[0].Copy.Src)
+			as.Equal("b", conf.Tasks[0].Copy.Dest)
+			as.Equal(TaskTypeCopy, conf.Tasks[0].TaskType())
+		}
+		{
+			as.Equal([]string{"ls", "ls -alh"}, conf.Tasks[1].Cmd.Commands)
+			as.Equal(TaskTypeCmd, conf.Tasks[1].TaskType())
+		}
+		{
+			as.Equal([]string{"ls"}, conf.Tasks[2].Cmd.Commands)
+			as.Equal(TaskTypeCmd, conf.Tasks[2].TaskType())
+		}
+		spew.Dump(conf)
+	})
+}
+
+func chTestDir() {
+	pwd, _ := os.Getwd()
+	if strings.HasSuffix(pwd, "/internal") {
+		_ = os.Chdir("..")
+	}
+}
